2d07fcc5900aff2d1564775391c97c0b3a5e030d
Mike Capecci <mjcapecci@gmail.com>
2020-05-17 12:19:45 -0500

    chanes prod database to connect with string

--- utils/db.js
+++ utils/db.js
@@ -11,11 +11,7 @@ const db = (dbMode = 'DEV'
       password: process.env.DB_DEV_PASSWORD,
       database: process.env.DB_DEV_DATABASE,
     })
-  : mysql.createConnection({
-      multipleStatements: true,
-      host: process.env.DB_PROD_HOST,
-      user: process.env.DB_PROD_USER,
-      password: process.env.DB_PROD_PASSWORD,
-      database: process.env.DB_PROD_DATABASE,
-    }));
+  : mysql.createConnection(
+      'mysql://mco6n42br6j1ghzz:uegxq3pt5rwf0s20@u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/cxd152txegnl3npj'
+    ));
 module.exports = db;
