abb16108efa2062b63187a4af528b364b5e8c322
Mike Capecci <mjcapecci@gmail.com>
2020-05-17 15:25:46 -0500

    deletes dev connection to sql for testing

--- utils/db.js
+++ utils/db.js
@@ -3,19 +3,11 @@ const mysql = require('mysql');
 let dbMode = process.env.NODE_ENV;
 
 // Create db connection
-const db = (dbMode = 'DEV'
-  ? mysql.createConnection({
-      multipleStatements: true,
-      host: process.env.DB_DEV_HOST,
-      user: process.env.DB_DEV_USER,
-      password: process.env.DB_DEV_PASSWORD,
-      database: process.env.DB_DEV_DATABASE,
-    })
-  : mysql.createConnection({
-      multipleStatements: true,
-      host: process.env.DB_PROD_HOST,
-      user: process.env.DB_PROD_USER,
-      password: process.env.DB_PROD_PASSWORD,
-      database: process.env.DB_PROD_DATABASE,
-    }));
+const db = mysql.createConnection({
+  multipleStatements: true,
+  host: process.env.DB_PROD_HOST,
+  user: process.env.DB_PROD_USER,
+  password: process.env.DB_PROD_PASSWORD,
+  database: process.env.DB_PROD_DATABASE,
+});
 module.exports = db;
