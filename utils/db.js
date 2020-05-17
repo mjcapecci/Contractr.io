63fd2c4df3d73a85f9140eb8bd6d3cb71a6bb2b8
Mike Capecci <mjcapecci@gmail.com>
2020-05-17 15:08:09 -0500

    adds correct db variables

--- utils/db.js
+++ utils/db.js
@@ -13,9 +13,9 @@ const db = (dbMode = 'DEV'
     })
   : mysql.createConnection({
       multipleStatements: true,
-      host: process.env.DB_DEV_HOST,
-      user: process.env.DB_DEV_USER,
-      password: process.env.DB_DEV_PASSWORD,
-      database: process.env.DB_DEV_DATABASE,
+      host: process.env.DB_PROD_HOST,
+      user: process.env.DB_PROD_USER,
+      password: process.env.DB_PROD_PASSWORD,
+      database: process.env.DB_PROD_DATABASE,
     }));
 module.exports = db;
